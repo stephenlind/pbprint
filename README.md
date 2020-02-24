a527767a8859eed9ced29955db011c38c7aec512
Stephen Lind <stephen.lind@gmail.com>
2020-02-23 22:05:54 -0800

    Update README, clean up comments and formatting

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # pbprint
-Automatically resize and print images from your clipboard
+`pbprint` automatically resizes images to be as large as possible on an A4 page, and then automatically prints them
+
+## Requirements
+
+Requires `pngpaste`, a tool for creating a png file out of the current copied item in your clipboard. 
+
+You can install `pngpaste` using [Homebrew](https://brew.sh/).
+
+`brew install pngpaste`
+
+Make sure `pngpaste` is in your `PATH`
+
+## Usage
+
+1. Copy an image to your clipboard
+2. Run `pbprint.sh`
+
+Without any arguments, this will resize the image to the clipboard to maximize it on an a4 page, and then print it using your default printer (via `lp`).
+
+`pbprint.sh --pdf` allows you to perform a dry-run by creating and open a pdf of the output so you can examine it before printing.
+
+## Motivation
+
+This script was written to automate the printing of coloring pages for kids. If you perform a [google image search](https://www.google.com/search?tbm=isch&q=coloring+pages+butterfly) for "coloring pages <something your kid likes>", you will get lots of great pictures, which can be copied to the clipboard. `pbprint.sh` makes the re-sizing and printing part much faster.
+
+
--- pbprint.sh
+++ pbprint.sh
@@ -1,18 +1,23 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-# A4 page at 300 dpi
+# Requirements
+# 1. pngpaste (brew install pngpaste)
+# 2. sips (preinstalled on macOS)
+# 3. lp (preinstalled on macOS, configured to use your default printer)
+
+# Constants: A4 page at 300 dpi
 LONG_EDGE=3508
 SHORT_EDGE=2480
 MARGIN=150 # 0.5 inches (0.25 on each side, safe for most printers)
 
-# copy the pasteboard to a png
+# copy the pasteboard to a png using pngpaste
 TEMP_PNG="pbprint.pbpaste.png"
 pngpaste ${TEMP_PNG}
 ORIG_HEIGHT=`identify -format '%h' ${TEMP_PNG}`
 ORIG_WIDTH=`identify -format '%w' ${TEMP_PNG}`
 
 # determine portrait/landscape
-if [[ ${ORIG_HEIGHT} > ${ORIG_WIDTH} ]]; then
+if [[ ${ORIG_HEIGHT} -ge ${ORIG_WIDTH} ]]; then
 	ORIENTATION="portrait"
 	WIDTH=${SHORT_EDGE}
 	HEIGHT=${LONG_EDGE}
@@ -24,25 +29,24 @@ else
 	EDGE_RATIO=`bc -l <<< "${ORIG_WIDTH} / ${ORIG_HEIGHT}"`
 fi
 
-# determine proper resample
+# determine proper maximum resample size for this image
 MAX_EDGE_RATIO=`bc -l <<< "${LONG_EDGE} / ${SHORT_EDGE}"`
 SQUARISH=`bc -l <<< "${EDGE_RATIO} < ${MAX_EDGE_RATIO}"`
 if [[ ${SQUARISH} ]]; then
 	# image is close enough to square that we need to use the short edge as limit
-	# to maximize 
+	# to maximize, multiply the short edge by the edge ratio
 	ADJUSTED_EDGE=`bc -l <<< "${EDGE_RATIO} * ${SHORT_EDGE} - ${MARGIN}"`
 	RESAMPLE_MAX=${ADJUSTED_EDGE%.*}
 else
+	# more asymetric than A4, can be resampled to the long edge
 	RESAMPLE_MAX=$(expr ${LONG_EDGE} - ${MARGIN})	
 fi	
 
-echo "Using orientation: ${ORIENTATION}"
-sips -Z ${RESAMPLE_MAX} \
--p ${HEIGHT} ${WIDTH} \
---padColor FFFFFF \
-"${TEMP_PNG}"
+# use sips to resize keeping aspect ratio 
+echo "Using orientation: ${ORIENTATION}, resizing image to ${RESAMPLE_MAX} pixels on the longest side"
+sips -Z ${RESAMPLE_MAX} -p ${HEIGHT} ${WIDTH} --padColor FFFFFF "${TEMP_PNG}"
 
-# output to pdf
+# convert png to pdf format
 TEMP_PDF="pbprint.output.pdf"
 sips -s dpiWidth 300 -s dpiHeight 300 -s format pdf "${TEMP_PNG}" --out "${TEMP_PDF}" 
 rm "${TEMP_PNG}"
@@ -53,7 +57,7 @@ if [[ "${1}" == "--pdf" ]]; then
 	mv ${TEMP_PDF} ${PDF_OUT}
 	open ${PDF_OUT}
 else
-	echo "printing"
+	echo "Printing in orientation ${ORIENTATION}"
 	lp -o ${ORIENTATION} -o fit-to-page "${TEMP_PDF}"	
 	rm "${TEMP_PDF}"
 fi
